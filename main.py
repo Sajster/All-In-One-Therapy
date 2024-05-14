272a2053928dcebd572e1fd0229b716051fb89a0
Sajen <sshah@sajenshah.com>
2024-05-14 14:39:52 -0700

    API Stuff 1

--- main.py
+++ main.py
@@ -1,1 +1,8 @@
-print("hi2")
+
+import google.generativeai as genai
+
+GOOGLE_API_KEY="AIzaSyDO3yKGP_m1bhXwBFJVeJrgdDmVigVDu98"
+genai.configure(api_key=GOOGLE_API_KEY)
+model = genai.GenerativeModel('gemini-pro')
+response = model.generate_content("Will Sin make the academic come back?")
+print(response.text)
